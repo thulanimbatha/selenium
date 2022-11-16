4ce915cf1ab116da2d6b466d00e55d60d63dd6b4
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-16 15:32:40 +0200

    added send_keys method, typing and entering into the searchbar

--- interaction.py
+++ interaction.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 
 service = Service("C:\Development\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get(url="https://en.wikipedia.org/wiki/Main_Page")
 # find element
-count_anchor = driver.find_element(by=By.CSS_SELECTOR, value="#articlecount a")
-# click on element
-count_anchor.click()
+# count_anchor = driver.find_element(by=By.CSS_SELECTOR, value="#articlecount a")
+# # click on element
+# count_anchor.click()
+
+search_bar = driver.find_element(by=By.NAME, value="search")
+search_bar.send_keys("Python")
+search_bar.send_keys(Keys.ENTER)
+
 driver.quit()
