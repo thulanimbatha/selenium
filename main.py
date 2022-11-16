f7920995496a320fd85a955e54c2da37fd2de9df
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-16 12:37:42 +0200

    fixed slight problem

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ driver.get("https://python.org")
 
 # find upcoming events and store them into a dictionary
 event_times = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget time")
-event_names = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget a")
+event_names = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget li a")
 
 for name in event_names:
     print(name.text)
