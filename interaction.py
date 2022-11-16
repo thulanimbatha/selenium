c8f9f3f2dffb4bf39b5f064846aa17d5bc11efc3
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-16 15:09:36 +0200

    clicked on element

--- interaction.py
+++ interaction.py
@@ -6,8 +6,8 @@ service = Service("C:\Development\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get(url="https://en.wikipedia.org/wiki/Main_Page")
-
+# find element
 count_anchor = driver.find_element(by=By.CSS_SELECTOR, value="#articlecount a")
-print(count_anchor.text)
-
+# click on element
+count_anchor.click()
 driver.quit()
